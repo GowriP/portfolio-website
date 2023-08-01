dee767ddda0601c2167becac5536e73d04499719
Gowri <prakashgowri72@gmail.com>
2023-08-01 16:16:25 -0400

    About, Experiences and Tech Completed

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,9 +1,61 @@
 import React from 'react'
+import { Tilt } from 'react-tilt';
+import { motion } from 'framer-motion';
 
-const About = () => {
+import { styles } from '../style';
+import { services } from '../constants';
+import { fadeIn, textVariant } from '../utils/motion';
+import { SectionWrapper } from '../hoc';
+
+const ServiceCard = ( {index, title, icon}) => {
   return (
-    <div>About</div>
+    <Tilt className="xs:w-[250px] w-full">
+      <motion.div
+        variants={(fadeIn("right", "spring", 0.5*index, 0.75))}
+        className='w-full green-pink-gradient p-[1px] rounded-[20px] shadow-card'>
+          <div
+            options = {{
+              max: 45,
+              scale: 1,
+              speed: 450
+            }} className='bg-tertiary rounded-[20px] py-5 px-12 min-h-[280px] flex 
+            justify-evenly items-center flex-col'>
+              <img src={icon} alt={title}
+              className='w-16 h-16 object-contain'/>
+              <h3 className='text-white text-[20px] font-bold text-center'>{title}</h3>
+          </div>
+      </motion.div>
+    </Tilt>
   )
 }
 
-export default About
+const About = () => {
+  return (
+    <>
+    <motion.div variants={textVariant()}>
+      <p className={styles.sectionSubText}>Introduction</p>
+      <h2 className={styles.sectionHeadText}>Overview.</h2>
+      </motion.div>
+
+      <motion.p
+        variants={fadeIn("","",0.1, 1)}
+        className='mt-2 text-secondary text-[17px] max-w-3xl leading-[30px]'
+      >
+        I am an aspiring Product Manager with an enthusiasm for continuous learning and a passion for crafting effective product strategies. I completed my MBA at Schulich School of Business in April, 2021.
+        <br /><br />
+        As a Full Stack Web Developer, my experiences have led me to deliver significant business outcomes and drive technology strategy on a large scale. Before pursuing my MBA, I worked at EFI as a web developer, where I developed applications to facilitate clients in printing and reproducing brand colors.
+        <br /><br />
+        Some noteworthy hobbies include exploring and cooking various cuisines, board games and hiking!
+        Connect with me on <a href="https://www.linkedin.com/in/gowri-prakash/" className="underline text-[#00cea8] text-400">LinkedIn</a> or email me at prakashgowri72@gmail.com
+      </motion.p>
+
+      <div className='mt-20 flex flex-wrap gap-10'>
+        {services.map((service, index) => (
+          <ServiceCard key={service.title} index={index} {...service} />
+        ))}
+      </div>
+    </>
+  )  
+}
+
+export default SectionWrapper(About, "about")
--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,8 +1,44 @@
-import React from 'react'
-
+import { motion } from 'framer-motion';
+import { styles } from '../style';
+import { ComputersCanvas } from './canvas';
 const Hero = () => {
   return (
-    <div>Hero</div>
+    <section className='relative w-full h-screen mx-auto'>
+      <div className={`${styles.paddingX} absolute inset-0 top-[40px] max-w-7xl mx-auto flex flex-row items-start gap-5`}>
+        <div className='flex flex-col justify-center items-center mt-5'>
+          <div className='w-5 h-5 rounded-full bg-[#05c880] '>
+            <div className='w-1 sm:h-80 h-40 mx-2 green-gradient'/>
+          </div>
+        </div>
+        <div>
+          <h1 className={`${styles.heroHeadText} text-white text-lg`}>Hi, I'm 
+            <span className='text-[#05c880]'> Gowri</span></h1>
+          <p className={`${styles.heroSubText} mt-2 text-white-100`}>
+            I am a recent MBA graduate with a background<br className='sm:block hidden'/>
+             in full stack software development
+          </p>
+        </div>
+      </div>
+      <ComputersCanvas/>
+      <div className='absolute xs:bottom-20 bottom-64 w-full flex justify-center items-center'>
+        <a href='#about'>
+          <div className='w-[35px] h-[64px] rounded-3xl border-4 border-secondary flex justify-center items-start p-2'>
+            <motion.dev
+              animate = {{
+                y : [0, 24, 0]
+              }}
+              transition = {{
+                duration: 1.5,
+                repeat: Infinity,
+                repeatType: 'loop' 
+              }}
+              className="w-3 h-3 rounded-full bg-secondary mb-1"
+              >
+            </motion.dev>
+          </div>
+        </a>
+      </div>
+    </section>
   )
 }
 
