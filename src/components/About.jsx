62c2f980e50383e9232884b0db14c77dcd153b81
Gowri <prakashgowri72@gmail.com>
2023-08-02 10:23:20 -0400

    EmailJS and Contact Completed

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -11,15 +11,14 @@ const ServiceCard = ( {index, title, icon}) => {
   return (
     <Tilt className="xs:w-[250px] w-full">
       <motion.div
-        variants={(fadeIn("right", "spring", 0.5*index, 0.75))}
+        variants={(fadeIn("right", "spring", index * 0.5, 0.75))}
         className='w-full green-pink-gradient p-[1px] rounded-[20px] shadow-card'>
           <div
             options = {{
               max: 45,
               scale: 1,
-              speed: 450
-            }} className='bg-tertiary rounded-[20px] py-5 px-12 min-h-[280px] flex 
-            justify-evenly items-center flex-col'>
+              speed: 450,
+            }} className='bg-tertiary rounded-[20px] py-5 px-12 min-h-[280px] flex justify-evenly items-center flex-col'>
               <img src={icon} alt={title}
               className='w-16 h-16 object-contain'/>
               <h3 className='text-white text-[20px] font-bold text-center'>{title}</h3>
@@ -49,7 +48,7 @@ const About = () => {
         Connect with me on <a href="https://www.linkedin.com/in/gowri-prakash/" className="underline text-[#00cea8] text-400">LinkedIn</a> or email me at prakashgowri72@gmail.com
       </motion.p>
 
-      <div className='mt-20 flex flex-wrap gap-10'>
+      <div className='mt-20 flex flex-wrap gap-10 justify-center'>
         {services.map((service, index) => (
           <ServiceCard key={service.title} index={index} {...service} />
         ))}
--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -17,13 +17,39 @@ const Contact = () => {
 
   const [loading, setLoading] = useState(false); 
   
-  const handleChange = (e) => {}
+  const handleChange = (e) => {
+    const {name, value} = e.target;
+    setForm({...form, [name]: value})
+  }
 
-  const handleSubmit = (e) => {}
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setLoading(true);
+    emailjs.send('service_zjk8jye',
+    'template_h2jpo1m',
+    {
+      from_name: form.name,
+      to_name: 'Gowri',
+      from_email: form.email,
+      message: form.message
+    },
+    'wmr4H1HokPLwp2HeK'
+    ).then(() => {
+      setLoading(false);
+      alert('Thank you. I will get back to you as soon as possible!')
+
+      setForm({name: '', email: '', message: ''})
+
+    }, (error)=> {
+      setLoading(false);
+      console.log(error);
+      alert('Something went wrong.');
+    })
+  }
 
   return (
-    <div className='xl:mt-12 xl:flex-row flex-row-reverse
-    flex gap-10 overflow-hidden'>
+    <div className='xl:mt-12 flex xl:flex-row flex-col-reverse
+      gap-10 overflow-hidden'>
       <motion.div
         variants = {slideIn('left','tween', 0.2, 1 )} 
         className='flex-[0.75] bg-black-100 p-8 rounded-2xl'>
