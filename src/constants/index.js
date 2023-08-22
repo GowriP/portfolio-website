0867eb0960b41f85711355c68a00d2fc09f68019
Gowri <prakashgowri72@gmail.com>
2023-08-22 17:07:35 -0400

    Removed Content Creator

--- src/constants/index.js
+++ src/constants/index.js
@@ -51,11 +51,7 @@ import {
     {
       title: "Backend Developer",
       icon: backend,
-    },
-    {
-      title: "Content Creator",
-      icon: creator,
-    },
+    }
   ];
   
   const technologies = [
